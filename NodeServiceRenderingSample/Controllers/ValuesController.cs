583cab38f426a12fe19b37043f5e4b57ef498bb3
Chan Wen <chawe@microsoft.com>
2019-01-21 17:02:02 +0800

    changes

--- NodeServiceRenderingSample/Controllers/ValuesController.cs
+++ NodeServiceRenderingSample/Controllers/ValuesController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.NodeServices;
 
 namespace NodeServiceRenderingSample.Controllers
 {
@@ -10,36 +11,20 @@ namespace NodeServiceRenderingSample.Controllers
     [ApiController]
     public class ValuesController : ControllerBase
     {
-        // GET api/values
-        [HttpGet]
-        public ActionResult<IEnumerable<string>> Get()
+        private readonly INodeServices nodeServices;
+
+        public ValuesController(INodeServices nodeServices)
         {
-            return new string[] { "value1", "value2" };
+            this.nodeServices = nodeServices;
+        }
+       
+        [HttpGet("render")]
+        public async Task<ActionResult<string>> RenderByNode(int id)
+        {
+            var htmlContent = await nodeServices.InvokeAsync<string>("./Node/renderMyNpm");
+            return Content(htmlContent, "text/html");
         }
 
-        // GET api/values/5
-        [HttpGet("{id}")]
-        public ActionResult<string> Get(int id)
-        {
-            return "value";
-        }
-
-        // POST api/values
-        [HttpPost]
-        public void Post([FromBody] string value)
-        {
-        }
-
-        // PUT api/values/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
-        {
-        }
-
-        // DELETE api/values/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
-        }
+      
     }
 }
